227da78288f85d8535d49a1225beaea795611341
jcayetano <jcayetano@apap.com.do>
2021-06-23 01:27:48 -0400

    Change name of tabs

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,10 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './home.page';
 import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './home-routing.module';
+import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
@@ -15,6 +15,6 @@ import { Tab1PageRoutingModule } from './home-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { MoviesResponse } from 'src/app/core/interfaces/movies-response';
+import { MoviesApiService } from '../../core/services/movieDb-api/movies-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class Tab1Page {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  movies: Array<MoviesResponse> = [];
+
+  constructor(private movesApiService: MoviesApiService) {
+
+  }
+
+  ngOnInit() {
+    this.getAllMovies();
+  }
+
+  async getAllMovies() {
+    await this.movesApiService.getMovies().subscribe(response => {
+      this.movies = response;
+      console.log(this.movies);
+    });
+  }
 
 }
